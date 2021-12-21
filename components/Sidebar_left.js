4e94f3988a741e2d152d959d2c9a678c47d33c99
David Richards <35054853+ravenOSS@users.noreply.github.com>
2021-12-21 11:41:46 -0700

    Remove incorrect styles subdirectory

--- components/Sidebar_left.js
+++
@@ -1,8 +0,0 @@
-import styles from '../styles/sideBar.module.css'
-export default function Sidebar_left() {
-	return (
-		<div>
-			<p>Sidebar Left</p>
-		</div>
-	)
-}
--- components/Sidebar_right.js
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/sideBar.module.css'
-
-export default function Sidebar_right() {
-	return (
-		<sidebar_right>
-			<p>Sidebar Right</p>
-		</sidebar_right>
-	)
-}
