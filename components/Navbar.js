6e5b4e9150d8ed323e2333f46dcc1c6dc68d6396
David Richards <35054853+ravenOSS@users.noreply.github.com>
2022-01-14 00:06:32 -0700

    Refactor Navbar for responsiveness wth flex

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,31 +2,36 @@ import Link from 'next/link'
 
 const Navbar = () => {
 	return (
-		<nav className='flex flex-wrap items-center content-end min-w-full py-4 mx-0 bg-gray-300 border-b-4 border-orange-400'>
-			<div className='ml-10 mr-auto text-2xl font-extrabold text-gray-600 '>
-				<h2>Douglas Richards</h2>
+		<nav className='flex flex-col items-center content-end min-w-full px-10 py-4 m-0 md:flex-row '>
+			{/* <nav className='flex flex-wrap items-center content-end min-w-full px-10 py-4 m-0 bg-gray-300 border-b-2 border-orange-400'> */}
+			<div className=' md:mr-auto'>
+				<h2 className='text-2xl font-extrabold text-gray-600'>
+					Douglas Richards
+				</h2>
 			</div>
-			<span className='w-48 bg-gray-500 '>
-				<Link href='/'>
-					<a className='mr-0 text-lg font-bold text-gray-600 no-underline hover:text-xl'>
-						About
-					</a>
-				</Link>
-			</span>
-			<span className='w-48 bg-gray-500 '>
-				<Link href='/bloglist'>
-					<a className='mr-0 text-lg font-bold text-gray-600 no-underline hover:text-xl'>
-						Blogs
-					</a>
-				</Link>
-			</span>
-			<span className='w-48 bg-gray-500 '>
-				<Link href='/profile'>
-					<a className='mr-0 text-lg font-bold text-gray-600 no-underline hover:text-xl'>
-						Author Profile
-					</a>
-				</Link>
-			</span>
+			<div className='flex flex-col md:flex-row'>
+				<div className='px-1 text-center w-36 '>
+					<Link href='/'>
+						<a className='text-lg font-bold text-gray-600 no-underline transition duration-300 hover:text-xl '>
+							About
+						</a>
+					</Link>
+				</div>
+				<div className='px-1 text-center w-36 '>
+					<Link href='/bloglist'>
+						<a className='text-lg font-bold text-gray-600 no-underline transition duration-300 hover:text-xl'>
+							Blogs
+						</a>
+					</Link>
+				</div>
+				<div className='px-1 text-center w-36'>
+					<Link href='/profile'>
+						<a className='text-lg font-bold text-gray-600 no-underline transition duration-300 hover:text-xl'>
+							Author Profile
+						</a>
+					</Link>
+				</div>
+			</div>
 		</nav>
 	)
 }
