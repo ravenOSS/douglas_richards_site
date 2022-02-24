54f8425fae071b61b90d5d92c713f6cf37092e39
David Richards <35054853+ravenOSS@users.noreply.github.com>
2022-02-23 18:09:36 -0700

    Modify layout to mesh with [id]

--- components/Layout.js
+++ components/Layout.js
@@ -3,10 +3,12 @@ import Footer from './Footer'
 
 export default function Layout({ children }) {
 	return (
-		<div className='flex flex-col items-center justify-center w-screen min-h-screen mx-auto bg-gray-400 dark:bg-gray-800 '>
-			<Navbar />
-			<main className='flex-grow'>{children}</main>
-			<Footer />
+		// <div className=' grid grid-cols-[minmax(360px,_1fr)]    w-full min-h-screen  bg-orange-500 dark:bg-orange-900 '>
+		<div className=' grid min-h-screen    w-full grid-cols-[minmax(360px,_1fr)] bg-gray-100 dark:bg-gray-700'>
+			<Navbar className='row-start-1 row-end-2 min-h-fit ' />
+			<main className='row-start-2 row-end-3'>{children}</main>
+			<Footer className='row-start-3 row-end-4  min-h-fit' />
 		</div>
 	)
 }
+// scrollbar scrollbar-thumb-green-900 scrollbar-track-blue-900
