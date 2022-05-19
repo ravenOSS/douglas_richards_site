d9231435b132b29a80595ef6578d62ec5de6c9cc
David Richards <35054853+ravenOSS@users.noreply.github.com>
2022-05-18 22:09:51 -0600

    Restrict content width

--- components/Layout.js
+++ components/Layout.js
@@ -3,10 +3,12 @@ import Footer from './Footer'
 
 export default function Layout({ children }) {
 	return (
-		<div className='grid-col-1 mx-auto grid w-screen items-center '>
+		<div className=' min-h-screen w-screen bg-grey-400 dark:bg-grey-500'>
+		<div className='flex flex-col mx-auto min-h-screen max-w-screen-lg items-center justify-center '>
 			<Navbar className=' min-h-fit ' />
-			<main className='flex flex-grow'>{children}</main>
+			<main className='flex-grow flex-col'>{children}</main>
 			<Footer className='  min-h-fit' />
+		</div>
 		</div>
 	)
 }
