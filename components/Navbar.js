12daa0802a33d3d8e078a19f89c34d119fd06a5d
David Richards <35054853+ravenOSS@users.noreply.github.com>
2022-05-18 22:10:26 -0600

    Add mobile menu dropdown

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import Link from 'next/link'
-import MobileMenu from './MobileMenu'
+// import MobileMenu from './MobileMenu'
+import { MenuIcon } from '@heroicons/react/solid'
 
 const navItems = [
 	{ url: '/', label: 'Home' },
@@ -7,15 +9,40 @@ const navItems = [
 	{ url: '/profile', label: 'Profile' },
 ]
 
+//! Wrap the burger in button to allow for click to open
+//! NO use a select dropdown for mobile
 export default function Navbar() {
+	const [isBurgerOpen, setIsBurgerOpen] = useState(false)
 	return (
-		<nav className='  w-screen bg-green-400 py-4 '>
-			<div className=' flex max-w-5xl   items-center justify-between bg-blue-300 px-6 dark:bg-blue-800'>
-				<div className='text-md font-extrabold text-gray-700 dark:text-gray-300 sm:text-lg md:text-2xl'>
+		<nav className='  flex w-full justify-center bg-green-400 py-4'>
+			<div className=' relative  flex  w-4/5 max-w-7xl items-center justify-between bg-blue-300 px-1 dark:bg-blue-800'>
+				<div className='text-sm font-extrabold text-gray-700 dark:text-gray-200 sm:text-lg md:text-2xl'>
 					Douglas Richards
 				</div>
-				<MobileMenu />
-				<div className='  hidden py-2 sm:flex'>
+				<div className='absolute top-0.5 right-1 flex flex-col items-end'>
+					<button onClick={() => setIsBurgerOpen((prev) => !prev)}>
+						<MenuIcon className='open-mobile-menu h-4 w-4 text-gray-700 dark:text-gray-200 sm:hidden' />
+					</button>
+					{isBurgerOpen && (
+						<div className='z-10 flex flex-col py-2 sm:hidden'>
+							{navItems.map(({ url, label }) => (
+								<div key={label} className='w-16 px-1 text-center md:w-24'>
+									<Link
+										href={url}
+										passHref
+										onClick={() => setIsBurgerOpen((prev) => !prev)}
+									>
+										<a className='sm:text-md text-sm font-bold text-gray-700 no-underline  dark:text-gray-300 md:text-lg '>
+											{label}
+										</a>
+									</Link>
+								</div>
+							))}
+						</div>
+					)}
+				</div>
+
+				<div className='desktop-menu hidden py-2 sm:flex'>
 					{navItems.map(({ url, label }) => (
 						<div key={label} className='w-16 px-1 text-center md:w-24'>
 							<Link href={url} passHref>
