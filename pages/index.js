70c24e3db51d2b2fc8f449d317264951c5256635
David Richards <35054853+ravenOSS@users.noreply.github.com>
2022-08-03 23:07:27 -0600

    feat: massage card layout to reduce size, improve readability

--- pages/index.js
+++ pages/index.js
@@ -24,8 +24,8 @@ export default function Home({ allPostsData }) {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<div className='flex   max-w-7xl flex-grow flex-col items-center justify-center  bg-gray-300  dark:bg-gray-700 '>
-				<h1 className='text-bold mx-auto my-4 text-center text-4xl font-bold  dark:text-gray-100 sm:text-2xl'>
-					What's going on here?
+				<h1 className='text-bold mx-auto my-4 text-center text-4xl font-bold  dark:text-orange-400 sm:text-2xl'>
+					A blog about Web technologies, IoT, and their confluence
 				</h1>
 				<section className='columns-1 gap-6 p-5 sm:columns-2 '>
 					<article className='bg-grey-200 prose text-black dark:text-gray-200'>
@@ -65,7 +65,7 @@ export default function Home({ allPostsData }) {
 					</article>
 				</section>
 				<section>
-					<div className='max-width-7xl  mx-auto grid auto-rows-min grid-cols-1 items-center  justify-center gap-4 bg-gray-300 px-2 dark:bg-gray-700 md:grid-cols-2'>
+					<div className='max-width-7xl  mx-auto grid auto-rows-fr grid-cols-1  gap-5 bg-gray-300 px-2 dark:bg-gray-700 sm:grid-cols-2'>
 						{allPostsData.map(
 							({
 								id,
@@ -83,20 +83,19 @@ export default function Home({ allPostsData }) {
 							}) => (
 								<article
 									key={id}
-									className='card my-2 grid h-full grid-cols-1 items-start justify-center gap-2 rounded-lg border-2  border-orange-400 p-1 text-gray-700 dark:text-gray-200 sm:grid-cols-2 sm:p-4 '
+									className='card grid max-h-full  grid-flow-row grid-cols-1   gap-5 rounded-lg border-2 border-orange-400  bg-transparent p-1 text-gray-700 dark:text-gray-200 sm:grid-cols-2 sm:p-2 '
 								>
-									{/* <div className=' flex flex-col divide-y-2 divide-solid text-gray-800  outline-1 dark:text-gray-200 sm:flex-row sm:divide-x sm:divide-none'> */}
-									<div className='col-span-1 col-start-1 my-2'>
-										<h4 className=' text-left text-xs '>{date}</h4>
-										<h2 className=' text-left text-lg font-bold '>{title}</h2>
+									<div className='col-span-1  col-start-1 mt-1  flex min-h-full flex-col place-items-start'>
+										<h4 className=' mb-5 text-xs '>{date}</h4>
+										<h2 className=' text-lg font-bold '>{title}</h2>
 									</div>
 
-									<div className=' relative col-span-1 col-start-1 my-2 gap-3 sm:col-span-1 sm:col-start-2'>
-										<p className='    line-clamp-2'>{issue}</p>
-										<p className='   line-clamp-2'>{solution}</p>
-										<div className='flex grow'></div>
+									<div className='  col-span-1 col-start-1  mt-1 flex min-h-full flex-col place-items-start sm:col-span-1 sm:col-start-2'>
+										<p className=' mb-5   line-clamp-2'>{issue}</p>
+										<p className='   mb-5 line-clamp-2'>{solution}</p>
+										<div className='grow'></div>
 										<Link className='' href={`/posts/${id}`} passHref>
-											<button className='w-25 m-1  h-10   bg-transparent px-2 py-1 text-sm font-semibold text-blue-700 no-underline  dark:text-blue-300'>
+											<button className=' w-35 mt-auto   h-10 bg-transparent  px-2  text-sm font-semibold tracking-wide text-blue-700 no-underline  dark:text-blue-300'>
 												Read More
 											</button>
 										</Link>
